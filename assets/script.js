8b01711e8ebde58f2e73e9ad3eceab9602192b3b
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-24 19:33:51 +0100

    fix image path

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
 
@@ -59,12 +59,12 @@ function getObjHtml(object) {
 
   const markup = `
   <div class="d-flex gap-2">
-   <div class="card mb-3" style="width: 450px; height: 125px;">
+   <div class="card">
             <div class="row g-0">
               <div class="col-md-4">
                 <img src="${img}" class="img-fluid rounded-start" alt="">
               </div>
-              <div class="col-md-8 bg-dark text-light">
+              <div class="col-md-8">
                 <div class="card-body">
                   <h5 id="card-name">${name}</h5>
                   <p id="card-role">${role}</p>
